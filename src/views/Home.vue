0d09e11c65a3bf5f6f596af72ac751f9457fb8d9
Katherine-dev <evorotnikowa@yandex.ru>
2021-09-28 00:38:24 +0300

    added examples of visibility props usage along with breakpoints

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,28 +1,12 @@
 <template>
  <div class="home">
     <h1>Homepage</h1>
-    <v-btn class="pink white--text mx-2">click me</v-btn>
-    <v-btn depressed dark color="pink" class="mr-2">click me</v-btn>
-    <v-btn text color="pink">click me</v-btn>
 
-    <v-btn depressed class="pink white--text mr-2">
-      <v-icon left>mdi-email</v-icon>
-      <span>email me</span>
-    </v-btn>
+    <!-- <v-btn class="hidden-md-and-down">click me</v-btn> -->
 
-    <v-btn depressed small class="pink white--text mr-2">
-      <v-icon left small>mdi-email</v-icon>
-      <span>email me</span>
-    </v-btn>
+    <!-- <v-btn class="hidden-md-and-up">click me</v-btn> -->
 
-    <v-btn depressed large class="pink white--text mr-2">
-      <span>email me</span>
-      <v-icon right large >mdi-email</v-icon>
-    </v-btn>
-
-    <v-btn fab depressed small dark color="purple">
-      <v-icon small>mdi-heart</v-icon>
-    </v-btn>
+    <v-btn class="hidden-sm-only">click me</v-btn>
   </div>
 </template>
 
