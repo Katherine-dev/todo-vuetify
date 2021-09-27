d494fa558296e668b5d0a6fd5c753bfcdf0925cf
Katherine-dev <evorotnikowa@yandex.ru>
2021-09-28 01:30:41 +0300

    overwrote default theme

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -4,4 +4,16 @@ import Vuetify from 'vuetify/lib/framework'
 Vue.use(Vuetify)
 
 export default new Vuetify({
+  icons: {
+    iconfont: 'mdi'
+
+  },
+
+  theme: {
+    themes: {
+      light: {
+        primary: '#9652ff'
+      }
+    }
+  }
 })
