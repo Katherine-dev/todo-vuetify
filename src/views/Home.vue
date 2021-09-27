e82d2a5b31a6d1c55a4d2b78bdaed1a06e864107
Katherine-dev <evorotnikowa@yandex.ru>
2021-09-27 23:46:45 +0300

    added some text with props

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,23 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+ <div class="home">
+    <h1>Homepage</h1>
+    <p class="red white--text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium modi perferendis nemo adipisci laboriosam magni, facere eum! Atque dolores et soluta, illo obcaecati culpa porro cupiditate nam minima quaerat corrupti.</p>
+    <p class="pink lighten-4 red--text text--darken-4">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti sunt quam placeat porro quis non cum aliquam, necessitatibus, officiis, eaque unde odit exercitationem consectetur quidem quasi odio error veritatis voluptate?</p>
+    <h1 class="display-4">Massive Display</h1>
+    <h4 class="display-1">Smaller Display</h4>
+    <p class="headline">This is a headline</p>
+    <p class="subheading font-weight-bold">This is a subheading</p>
+    <p class="caption">This is caption</p>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator'
-import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
+import Vue from 'vue'
 
-@Component({
+export default Vue.extend({
+  name: 'Home',
+
   components: {
-    HelloWorld
   }
 })
-export default class Home extends Vue {}
 </script>
