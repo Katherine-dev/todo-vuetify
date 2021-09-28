1788495df5b7d4a0541e8a8825947dca9aef0366
Katherine-dev <evorotnikowa@yandex.ru>
2021-09-28 17:43:58 +0300

    grid practice

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,8 +1,30 @@
 <template>
- <div class="dashboard">
+ <div class="dashboard" style="height:100%">
     <v-subheader class="grey--text">Dashboard</v-subheader>
-      <v-container class="my-5 mx-4">
-        content
+      <v-container fill-height class="my-5 mx-4" style="align-content: flex-start">
+        <v-row wrap no-gutters>
+          <v-col cols="12" md="6">
+            <v-btn outlined block color="primary" >1</v-btn>
+          </v-col>
+          <v-col cols="4" md="2">
+            <v-btn outlined block color="primary" >3</v-btn>
+          </v-col>
+          <v-col cols="4" md="2">
+            <v-btn outlined block color="primary" >4</v-btn>
+          </v-col>
+          <v-col cols="4" md="2">
+            <v-btn outlined block color="primary" >5</v-btn>
+          </v-col>
+        </v-row>
+
+        <v-row  wrap no-gutters justify="space-around" class="mt-4">
+          <v-col cols="4" md="3">
+            <v-btn outlined block color="success">1</v-btn>
+          </v-col>
+          <v-col cols="4" md="3">
+            <v-btn outlined block color="success">2</v-btn>
+          </v-col>
+        </v-row>
       </v-container>
   </div>
 </template>
